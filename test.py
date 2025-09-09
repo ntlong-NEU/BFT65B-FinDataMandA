5a62fd051bf19e0233c0481fdc963d40d823d1e8
Nguyen Tuan Long <ntlong@neu.edu.vn>
2025-09-09 21:22:24 +0700

    99

--- test.py
+++ test.py
@@ -1,1 +1,10 @@
-print("Hello BFT65B")
+text = ' learning Machine learning models are get rules from data  data'
+
+fre_dict = dict()
+for w in text.split():
+    if w in fre_dict:
+        fre_dict[w] += 1
+    else:
+        fre_dict[w] = 1
+print(fre_dict)
+
